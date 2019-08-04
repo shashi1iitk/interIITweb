c5cf079e33c9ed048581d4148ab0a308bcacca29
Shashi <shashi1iitk@gmail.com>
2019-08-04 15:46:46 +0530

    Scorecard, Live score complete

--- main.py
+++ main.py
@@ -72,6 +72,55 @@ class Match(db.Model):
     commentry = db.Column(db.String(10000), nullable=False)
     lineup1 = db.Column(db.String(1000), nullable=False)
 
+class Sports(db.Model):
+    __tablename__ = 'sports'
+    id = db.Column(db.Integer, primary_key=True)
+    sports_name = db.Column(db.String(200), nullable=False)
+    category = db.Column(db.String(1), nullable=False)
+    max_player = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+
+class College(db.Model):
+    __tablename__ = 'college'
+    id = db.Column(db.Integer, primary_key=True)
+    clg_name = db.Column(db.String(400), nullable=False)
+    clg_nickname = db.Column(db.String(200), nullable=False)
+    logo_url = db.Column(db.String(600), nullable=False)
+
+class Point_master(db.Model):
+    __tablename__ = 'point_master'
+    id = db.Column(db.Integer, primary_key=True)
+    clg_id = db.Column(db.Integer(), nullable=False)
+    points = db.Column(db.Integer(), nullable=False)
+
+class Point_main(db.Model):
+    __tablename__ = 'point_main'
+    id = db.Column(db.Integer, primary_key=True)
+    sports_id = db.Column(db.Integer(), nullable=False)
+    c_1 = db.Column(db.Integer(), nullable=False)
+    c_2 = db.Column(db.Integer(), nullable=False)
+    c_3 = db.Column(db.Integer(), nullable=False)
+    c_4 = db.Column(db.Integer(), nullable=False)
+    c_5 = db.Column(db.Integer(), nullable=False)
+    c_6 = db.Column(db.Integer(), nullable=False)
+    c_7 = db.Column(db.Integer(), nullable=False)
+    c_8 = db.Column(db.Integer(), nullable=False)
+    c_9 = db.Column(db.Integer(), nullable=False)
+    c_10 = db.Column(db.Integer(), nullable=False)
+    c_11 = db.Column(db.Integer(), nullable=False)
+    c_12 = db.Column(db.Integer(), nullable=False)
+    c_13 = db.Column(db.Integer(), nullable=False)
+    c_14 = db.Column(db.Integer(), nullable=False)
+    c_15 = db.Column(db.Integer(), nullable=False)
+    c_16 = db.Column(db.Integer(), nullable=False)
+    c_17 = db.Column(db.Integer(), nullable=False)
+    c_18 = db.Column(db.Integer(), nullable=False)
+    c_19 = db.Column(db.Integer(), nullable=False)
+    c_20 = db.Column(db.Integer(), nullable=False)
+    c_21 = db.Column(db.Integer(), nullable=False)
+    c_22 = db.Column(db.Integer(), nullable=False)
+    c_23 = db.Column(db.Integer(), nullable=False)
+
 
 @app.route("/")
 def home():
@@ -165,15 +214,53 @@ def register0():
     return render_template('register.html', params=params)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     #return "Blank"
-    return render_template('login.html', params=params)
+
+    if (request.method == 'POST'):
+        print('Hello')
+        email = request.form['email']
+        password = request.form['password']
+        hash = oracle10.hash(password, user="player")
+        return hash
+    else:
+        return render_template('login.html', params=params)
 
 
-@app.route("/scorecard")
+@app.route("/scorecard", methods=['GET', 'POST'])
 def scorecard():
-    return render_template('scorecard.html', params=params)
+    if request.method == 'POST':
+        sports_id = request.form['sport_id']
+        if (sports_id=='0'):
+            points = Point_master.query.all()
+            points_master = []
+            for point in points:
+                points_dict = {"clg_id": point.clg_id, "point": point.points}
+                points_master.append(points_dict)
+            print(points_master)
+            return json.dumps(points_master)
+        else:
+            print(type(int(sports_id)))
+            sport_points = Point_main.query.filter(Point_main.sports_id == int(sports_id)).all()
+            for sport_point in sport_points:
+                point_dict = {"c_1": sport_point.c_1, "c_2": sport_point.c_2,  "c_3": sport_point.c_3, "c_4": sport_point.c_4, "c_5": sport_point.c_5, "c_6": sport_point.c_6, "c_7": sport_point.c_7, "c_8": sport_point.c_8, "c_9": sport_point.c_9, "c_10": sport_point.c_10, "c_11": sport_point.c_11, "c_12": sport_point.c_12, "c_13": sport_point.c_13, "c_14": sport_point.c_14, "c_15": sport_point.c_15, "c_16": sport_point.c_16, "c_17": sport_point.c_17, "c_18": sport_point.c_18, "c_19": sport_point.c_19, "c_20": sport_point.c_20, "c_21": sport_point.c_21, "c_22": sport_point.c_22, "c_23": sport_point.c_23}
+            print(point_dict)
+            return json.dumps(point_dict)
+    else:
+        sports = Sports.query.all()
+        sports_list= []
+        for sport in sports:
+            sports_dict = {"id": sport.id, "sport_name": sport.sports_name, "category": sport.category}
+            sports_list.append(sports_dict)
+        print(sports_list)
+        colleges = College.query.all()
+        college_list = []
+        for college in colleges:
+            college_dict = {"id": college.id, "college_name": college.clg_name, "college_nickname": college.clg_nickname, "college_logo": college.logo_url}
+            college_list.append(college_dict)
+        print(college_list)
+        return render_template('scorecard.html', params=params, sports_list = sports_list, college_list = college_list)
 
 
 @app.route("/livescore")
@@ -205,19 +292,12 @@ def allowed_file(filename):
 @app.route('/see')
 def see():
     stds = Players.query.all()
-    print(stds[0].name)
-    return "Blank"
+    print(stds[1].name)
+    return stds[1].name
 
 @app.route('/getLiveMatches_Ajax', methods=['GET','POST'])
 def getLiveMatches_Ajax():
     #live_ids = request.form('live_id')
-    # data = request.json
-    #ids of live matches
-    #enter query to get ids of current live matches
-    #if(compare live_ids with ids of current live matches)
-    #if there is a mismatch send ''mismatch'
-
-    #else
     #print(live_ids)
     time_now = datetime.now()
 
